ac3088fa1f375f2a94971d8a8ebed5c02ae29bd5
RAHUL KUMAR <84653100+rahulkumar1404@users.noreply.github.com>
2022-06-29 12:31:37 +0530

    Create 010 Add two numbers represented by linked lists .cpp

---
+++ Practice/Linked List/Singly Linked/010 Add two numbers represented by linked lists .cpp
@@ -0,0 +1,150 @@
+/*
+https://practice.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1#
+*/
+// { Driver Code Starts
+// driver
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+ // } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    private:
+    Node* rev(Node* head){
+        Node* curr = head;
+        Node* next = NULL;
+        Node* prev = NULL;
+        while(curr!= NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    void insert(struct Node* &head,struct Node* &tail,int val){
+        Node* temp = new Node(val);
+        if(head==NULL){
+            head = temp;
+            tail = temp;
+            return;
+        }else{
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+    struct Node* add(struct Node* first, struct Node* second){
+        Node* ansHead = NULL;
+        Node* ansTail = NULL;
+        int carry = 0;
+        while(first!=NULL||second!=NULL||carry!=0){
+            int val1 = 0;
+            if(first!=NULL){
+                val1 = first->data;
+            }
+            int val2 = 0;
+            if(second!=NULL){
+                val2 = second->data;
+            }
+            int sum = val1+val2+carry;
+            int digit = sum%10;
+            insert(ansHead,ansTail,digit);
+            carry = sum/10;
+            if(first!=NULL){
+                first = first->next;
+            }
+            if(second!=NULL){
+                second = second->next;
+            }
+        }
+        return ansHead;
+    }
+    public:
+    //Function to add two numbers represented by linked list.
+    struct Node* addTwoLists(struct Node* first, struct Node* second)
+    {
+        // code here
+        first = rev(first);
+        second = rev(second);
+        Node* ans = add(first,second);
+        ans = rev(ans);
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution ob;
+        Node* res = ob.addTwoLists(first,second);
+        printList(res);
+    }
+    return 0;
+}
+  // } Driver Code Ends
