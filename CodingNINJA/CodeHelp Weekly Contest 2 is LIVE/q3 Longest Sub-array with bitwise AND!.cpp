88a003e86977b23616ff45ffccb90cffe5978e1c
RAHUL KUMAR <84653100+rahulkumar1404@users.noreply.github.com>
2022-07-09 11:27:41 +0530

    Create q3 Longest Sub-array with bitwise AND!.cpp

---
+++ CodingNINJA/CodeHelp Weekly Contest 2 is LIVE/q3 Longest Sub-array with bitwise AND!.cpp
@@ -0,0 +1,22 @@
+/*
+https://www.codingninjas.com/codestudio/contests/love-babbar-contest-ii/3583551/problems/14818
+*/
+vector<int> bitwiseAnd(int n, int q, vector<int> arr, vector<int> queries)
+{
+    for(int i = 1;i<n;i++){
+        arr[i] = arr[i]&arr[i-1];
+    }
+    vector<int> ans;
+    int qInd = 0;
+    while(q--){
+        int max = 0;
+            for(int i = 0;i<n;i++){
+            if(arr[i] >= queries[qInd]){
+                max = i+1;
+            }
+        }
+            ans.push_back(max);
+            qInd++;
+    }
+    return ans;
+}
