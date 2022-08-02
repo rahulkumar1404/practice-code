f8604dd69ee90dc06a30629572681a38d08b789e
RAHUL KUMAR <84653100+rahulkumar1404@users.noreply.github.com>
2022-08-02 22:16:29 +0530

    Update 012 Merge Two BST.cpp

--- Practice/Tree/Binary Search Tree/012 Merge Two BST.cpp
+++ Practice/Tree/Binary Search Tree/012 Merge Two BST.cpp
@@ -90,3 +90,119 @@ step 2 : merge two sorted LinkedList
 step 3 : sorted linkedlist to BST
 T.c O(m+n) & S.C O(h1+h2)
 */
+/*************************************************************
+    
+    Following is the Binary Tree node structure:
+
+    class TreeNode{
+
+        public :
+            int data;
+            TreeNode *left;
+            TreeNode *right;
+
+            TreeNode(int data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+            }
+
+            ~TreeNode() {
+            if (left){
+            delete left;
+            }
+
+            if (right){
+            delete right;
+            }
+        }   
+    };
+
+*************************************************************/
+void convertIntoSortedDoublyLinkedList(TreeNode<int>* &root,TreeNode<int>*& head){
+    if(root == NULL)
+        return ;
+    convertIntoSortedDoublyLinkedList(root->right,head);
+    root->right = head;
+    if(head != NULL)
+        head->left = root;
+    head = root;
+    convertIntoSortedDoublyLinkedList(root->left,head);
+}
+TreeNode<int>* mergeTwoLinkedList(TreeNode<int>* &head1,TreeNode<int>*& head2){
+    TreeNode<int>* head = NULL;
+    TreeNode<int>* tail = NULL;
+    while(head1 != NULL && head2 != NULL){
+        if(head1->data < head2->data){
+            if(head == NULL){
+                head = head1;
+                tail = head1;
+                head1 = head1->right;
+                
+            }else{
+                tail->right = head1;
+                head1->left = tail;
+                tail = head1;
+                head1 = head1->right;
+            }
+        }else{
+            if(head == NULL){
+                head = head2;
+                tail = head2;
+                head2 = head2->right;
+                
+            }else{
+                tail->right = head2;
+                head2->left = tail;
+                tail = head2;
+                head2 = head2->right;
+            }
+        }
+    }
+    while(head1 != NULL){
+        tail->right = head1;
+        head1->left = tail;
+        tail = head1;
+        head1 = head1->right;
+    }
+    while(head2 != NULL){
+        tail->right = head2;
+        head2->left = tail;
+        tail = head2;
+        head2 = head2->right;
+    }
+    return head;
+}
+int countNodes(TreeNode<int>* &head){
+    int cnt = 0;
+    TreeNode<int>* temp = head;
+    while(temp != NULL){
+        cnt++;
+        temp = temp->right;
+    }
+    return cnt;
+}
+TreeNode<int>* linkedListToBST(TreeNode<int>* &head,int n){
+    if(n <= 0 || head == NULL)
+        return NULL;
+    TreeNode<int>* left = linkedListToBST(head,n/2);
+    
+    TreeNode<int>* root = head;
+    root->left = left;
+    head = head->right;
+    root->right = linkedListToBST(head,n - n/2 -1);
+    return root;
+}
+TreeNode<int> *mergeBST(TreeNode<int> *root1, TreeNode<int> *root2){
+    // Write your code here.
+    TreeNode<int>* head1 = NULL;
+    convertIntoSortedDoublyLinkedList(root1,head1);
+    head1->left = NULL;
+    TreeNode<int>* head2 = NULL;
+    convertIntoSortedDoublyLinkedList(root2,head2);
+    head2->left = NULL;
+    
+    TreeNode<int>* head = mergeTwoLinkedList(head1,head2);
+    
+    return linkedListToBST(head,countNodes(head));
+}
